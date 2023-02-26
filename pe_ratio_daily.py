7464a83a4d91d4211086320519071ca124878227
Dennis Lan <kueiyulan@gmail.com>
2023-02-26 22:25:37 +0800

    Init FCM service

--- pe_ratio_daily.py
+++ pe_ratio_daily.py
@@ -1,6 +1,12 @@
 import requests
 import pandas as pd
 
+import Credentials.firebase_cred as cred_path
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import messaging
+
 # Define the URL for the API endpoint
 url = "https://www.twse.com.tw/exchangeReport/BWIBBU_d"
 
@@ -32,5 +38,24 @@ except KeyError:
 df = pd.DataFrame(rows, columns=columns)
 
 # Output the DataFrame to a CSV file
-df.to_csv("pe_ratios.csv", index=False, encoding="utf_8_sig")
+# df.to_csv("pe_ratios.csv", index=False, encoding="utf_8_sig")
 
+# Initialize the Firebase Admin SDK with your service account credentials
+cred = credentials.Certificate(cred_path.PATH)
+
+firebase_admin.initialize_app(cred)
+
+# Define your notification content
+message = messaging.Message(
+    notification=messaging.Notification(
+        title="Test notification title",
+        body="Test notification message"
+    ),
+    topic="YOUR_TOPIC"
+)
+
+# Send the notification
+response = messaging.send(message)
+
+# Print the response from FCM
+print(response)
